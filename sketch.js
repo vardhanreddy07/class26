aff54ccf0fa2967ddc0ecf6e3940e7abca86f2eb
vardhan <bvardhanreddy07@gmail.com>
2021-06-08 13:53:55 +0530

    added box

--- sketch.js
+++ sketch.js
@@ -33,6 +33,8 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    
+    box6 = new Box(810,160,70,70);
 
     bird = new Bird(100,100);
 
@@ -56,6 +58,7 @@ function draw(){
     log3.display();
 
     box5.display();
+    box6.display();
     log4.display();
     log5.display();
 
